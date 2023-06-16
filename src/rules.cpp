ee48158adfd8d5c194c8280ea8d9f957894136dd
Serge Johanns <serge.johanns@gmail.com>
2023-06-16 03:38:05 +0200

    Apply final fixes frame rule

--- src/rules.cpp
+++ src/rules.cpp
@@ -95,7 +95,7 @@ namespace desyl
         std::vector<std::pair<size_t, size_t>> overlap;
         for (size_t i = 0; i < precondition.size(); ++i)
         {
-            for (size_t j = 0; j < precondition.size(); ++j)
+            for (size_t j = 0; j < postcondition.size(); ++j)
             {
                 if (precondition[i] == postcondition[j])
                 {
@@ -111,7 +111,7 @@ namespace desyl
                 term_vars.begin(), term_vars.end(),
                 existentials.begin(), existentials.end(),
                 std::inserter(intersection, intersection.end()));
-            if (intersection.size() == 0)
+            if (intersection.size() != 0)
             {
                 continue;
             }
@@ -125,17 +125,28 @@ namespace desyl
         auto new_goal(goal);
         auto const existentials = new_goal.variables().existentials();
 
-        auto precondition_pointers = new_goal.spec.precondition.heap.pointer_declarations;
-        auto postcondition_pointers = new_goal.spec.postcondition.heap.pointer_declarations;
+        auto &precondition_pointers = new_goal.spec.precondition.heap.pointer_declarations;
+        auto &postcondition_pointers = new_goal.spec.postcondition.heap.pointer_declarations;
+        size_t pointer_size = precondition_pointers.size();
         remove_frame_overlap(existentials, precondition_pointers, postcondition_pointers);
+        bool pointer_changed = pointer_size != precondition_pointers.size();
 
-        auto precondition_arrays = new_goal.spec.precondition.heap.array_declarations;
-        auto postcondition_arrays = new_goal.spec.postcondition.heap.array_declarations;
+        auto &precondition_arrays = new_goal.spec.precondition.heap.array_declarations;
+        auto &postcondition_arrays = new_goal.spec.postcondition.heap.array_declarations;
+        size_t array_size = precondition_pointers.size();
         remove_frame_overlap(existentials, precondition_arrays, postcondition_arrays);
+        bool array_changed = array_size != precondition_arrays.size();
 
-        auto precondition_predicates = new_goal.spec.precondition.heap.predicate_calls;
-        auto postcondition_predicates = new_goal.spec.postcondition.heap.predicate_calls;
+        auto &precondition_predicates = new_goal.spec.precondition.heap.predicate_calls;
+        auto &postcondition_predicates = new_goal.spec.postcondition.heap.predicate_calls;
+        size_t predicate_size = precondition_pointers.size();
         remove_frame_overlap(existentials, precondition_predicates, postcondition_predicates);
+        bool predicate_changed = predicate_size != precondition_predicates.size();
+
+        if (!pointer_changed && !array_changed && !predicate_changed)
+        {
+            return std::vector<Derivation>{};
+        }
 
         Derivation deriv{
             .goals = std::vector<Goal>{new_goal},
