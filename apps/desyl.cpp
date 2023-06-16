59c74d322248e222bfe914b0bc02c5eb4f3ff80a
Serge Johanns <serge.johanns@gmail.com>
2023-06-16 03:44:03 +0200

    Fix frame test case

--- apps/desyl.cpp
+++ apps/desyl.cpp
@@ -4,7 +4,7 @@
 
 std::string swap = "{true; <x,  0> -> a * <y, 0> -> b;} swap(loc x, loc y) {true; <x, 0> -> b * <y, 0> -> a;}";
 std::string empt = "{true;emp;} empt() {true;emp;}";
-std::string fram = "{true; <x, 0> -> a;} empt() {true; <x, 0> -> a * <y, 0> -> b;}";
+std::string fram = "{true; <x, 0> -> a;} frame() {true; <x, 0> -> a;}";
 
 int main()
 {
