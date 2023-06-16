c6526678f45bce5347920c579e5bd44b4a556f4c
Serge Johanns <serge.johanns@gmail.com>
2023-06-16 14:43:28 +0200

    Add more extensive test cases

--- apps/desyl.cpp
+++ apps/desyl.cpp
@@ -6,10 +6,14 @@ std::string empt = "{true;emp;} empt() {true;emp;}";
 std::string fram = "{true; <x, 0> -> a;} frame() {true; <x, 0> -> a;}";
 std::string writ = "{true; <x, 0> -> a;} frame(int b) {true; <x, 0> -> b;}";
 std::string swap = "{true; <x,  0> -> a * <y, 0> -> b;} swap(loc x, loc y) {true; <x, 0> -> b * <y, 0> -> a;}";
+std::string three_swap = "{true; <x,  0> -> a * <y, 0> -> b * <z, 0> -> c;} swap(loc x, loc y, loc z) {true; <x, 0> -> b * <y, 0> -> c * <z, 0> -> a;}";
+std::string four_swap = "{true; <x,  0> -> a * <y, 0> -> b * <z, 0> -> c * <zz, 0> -> d;} swap(loc x, loc y, loc z, loc zz) {true; <x, 0> -> b * <y, 0> -> c * <z, 0> -> d * <zz, 0> -> a;}";
+// Requires unifyheaps
+// std::string notsure = "{true; <x,  0> -> a * <y, 0> -> b;} notsure(loc x, loc y) {true; <x, 0> -> c * <c, 0> -> 0;}";
 
 int main()
 {
-    desyl::Query query = desyl::parse(swap);
+    desyl::Query query = desyl::parse(four_swap);
     desyl::synthesize(std::move(query));
     return 0;
 }
