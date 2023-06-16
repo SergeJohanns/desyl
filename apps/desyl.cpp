ab35c6fff97b6c2a9f11347182f880aa8aa7ad08
Serge Johanns <serge.johanns@gmail.com>
2023-06-16 02:50:09 +0200

    Add test case for frame rule

--- apps/desyl.cpp
+++ apps/desyl.cpp
@@ -4,12 +4,14 @@
 
 std::string swap = "{true; <x,  0> -> a * <y, 0> -> b;} swap(loc x, loc y) {true; <x, 0> -> b * <y, 0> -> a;}";
 std::string empt = "{true;emp;} empt() {true;emp;}";
+std::string fram = "{true; <x, 0> -> a;} empt() {true; <x, 0> -> a * <y, 0> -> b;}";
 
 int main()
 {
     std::cout << desyl::example("!f => 4 == 5  < (-54 <= !-4)") << std::endl;
     // desyl::Query query = desyl::parse(swap);
-    desyl::Query query = desyl::parse(empt);
+    // desyl::Query query = desyl::parse(empt);
+    desyl::Query query = desyl::parse(fram);
     desyl::synthesize(std::move(query));
     return 0;
 }
