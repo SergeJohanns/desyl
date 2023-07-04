023c429ba029a538a4bb3a561025adb8e5b8beba
Serge Johanns <serge.johanns@gmail.com>
2023-07-04 11:14:31 +0200

    Add more examples

--- apps/desyl.cpp
+++ apps/desyl.cpp
@@ -14,14 +14,17 @@ std::string three_swap = "{true; <x,  0> -> a * <y, 0> -> b * <z, 0> -> c;} swap
 std::string four_swap = "{true; <x,  0> -> a * <y, 0> -> b * <z, 0> -> c * <zz, 0> -> d;} swap(loc x, loc y, loc z, loc zz) {true; <x, 0> -> b * <y, 0> -> c * <z, 0> -> d * <zz, 0> -> a;}";
 // Requires unifyheaps
 std::string pick = "{true; <x,  0> -> 239 * <y, 0> -> 30;} pick(loc x, loc y) {true; <x, 0> -> z * <y, 0> -> z;}";
+std::string three_pick = "{true; <x,  0> -> 239 * <y, 0> -> 30 * <z, 0> -> 42;} pick(loc x, loc y, loc z) {true; <x, 0> -> n * <y, 0> -> n * <z, 0> -> n;}";
 // Requires pick
-std::string notsure = "{true; <x,  0> -> a * <y, 0> -> b;} notsure(loc x, loc y) {true; <x, 0> -> c * <c, 0> -> 0;}";
+std::string notsure = "{true; <x,  0> -> a * <y, 0> -> b * <z, 0> -> null;} notsure(loc x, loc y, loc z) {true; <x, 0> -> c * <c, 0> -> 0 * <z, 0> -> c;}";
 // Requires branch?
-std::string max = "{ true; <r, 0> -> null; } max(loc r, int x, int y) { x <= m || y <= m; <r, 0> -> m; }";
+std::string max = "{ true; <r, 0> -> null; } max(loc r, int x, int y) { x <= m && y <= m; <r, 0> -> m; }";
+// Shouldn't find any clauses even with branch
+std::string three_max = "{ true; <r, 0> -> null; } max(loc r, int x, int y) { x <= m && y <= m && z <= m; <r, 0> -> m; }";
 
 int main()
 {
-    desyl::Query query = desyl::parse(max);
+    desyl::Query query = desyl::parse(notsure);
     desyl::synthesize(std::move(query));
     return 0;
 }
