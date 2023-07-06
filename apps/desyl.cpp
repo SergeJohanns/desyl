7437f2d5a9b92905a5c78d2a03cede28574d0fe5
Serge Johanns <serge.johanns@gmail.com>
2023-07-07 01:40:53 +0200

    Fix examples

--- apps/desyl.cpp
+++ apps/desyl.cpp
@@ -3,11 +3,11 @@
 #include <desyl/ast.hpp>
 
 // Requires emp
-std::string empt = "{true;emp;} empt() {true;emp;}";
+std::string empty = "{true;emp;} empty() {true;emp;}";
 // Requires frame
-std::string fram = "{true; <x, 0> -> a;} frame() {true; <x, 0> -> a;}";
+std::string frame = "{true; <x, 0> -> a;} frame() {true; <x, 0> -> a;}";
 // Requires write
-std::string writ = "{true; <x, 0> -> a;} frame(int b) {true; <x, 0> -> b;}";
+std::string write = "{true; <x, 0> -> a;} write(int b) {true; <x, 0> -> b;}";
 // Requires read
 std::string swap = "{true; <x,  0> -> a * <y, 0> -> b;} swap(loc x, loc y) {true; <x, 0> -> b * <y, 0> -> a;}";
 std::string three_swap = "{true; <x,  0> -> a * <y, 0> -> b * <z, 0> -> c;} swap(loc x, loc y, loc z) {true; <x, 0> -> b * <y, 0> -> c * <z, 0> -> a;}";
@@ -16,11 +16,12 @@ std::string four_swap = "{true; <x,  0> -> a * <y, 0> -> b * <z, 0> -> c * <zz, 
 std::string pick = "{true; <x,  0> -> 239 * <y, 0> -> 30;} pick(loc x, loc y) {true; <x, 0> -> z * <y, 0> -> z;}";
 std::string three_pick = "{true; <x,  0> -> 239 * <y, 0> -> 30 * <z, 0> -> 42;} pick(loc x, loc y, loc z) {true; <x, 0> -> n * <y, 0> -> n * <z, 0> -> n;}";
 // Requires pick
-std::string notsure = "{true; <x,  0> -> a * <y, 0> -> b * <z, 0> -> null;} notsure(loc x, loc y, loc z) {true; <x, 0> -> c * <c, 0> -> 0 * <z, 0> -> c;}";
+std::string notsure = "{true; <x,  0> -> a * <y, 0> -> b;} notsure(loc x, loc y) {true; <x, 0> -> c * <c, 0> -> 0;}";
+std::string three_notsure = "{true; <x,  0> -> a * <y, 0> -> b * <z, 0> -> null;} notsure(loc x, loc y, loc z) {true; <x, 0> -> c * <c, 0> -> 0 * <z, 0> -> c;}";
 // Requires branch?
-std::string max = "{ true; <r, 0> -> null; } max(loc r, int x, int y) { x <= m && y <= m; <r, 0> -> m; }";
+std::string max = "{true; <r, 0> -> null;} max(loc r, int x, int y) { x <= m && y <= m; <r, 0> -> m;}";
 // Shouldn't find any clauses even with branch
-std::string three_max = "{ true; <r, 0> -> null; } max(loc r, int x, int y) { x <= m && y <= m && z <= m; <r, 0> -> m; }";
+std::string three_max = "{true; <r, 0> -> null;} max(loc r, int x, int y) { x <= m && y <= m && z <= m; <r, 0> -> m;}";
 
 int main()
 {
