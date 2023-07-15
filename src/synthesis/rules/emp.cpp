1f2607eb15099603b512140ef85be037e766724c
Serge Johanns <serge.johanns@gmail.com>
2023-07-15 15:40:49 +0200

    Strengthen EMPTY condition

--- src/synthesis/rules/emp.cpp
+++ src/synthesis/rules/emp.cpp
@@ -7,8 +7,10 @@ namespace desyl
 {
     std::vector<Derivation> EmpRule::apply(Goal const &goal) const
     {
-        auto const &precondition_empty = std::move(goal).spec.precondition.heap.pointer_declarations.size() == 0;
-        auto const &postcondition_empty = std::move(goal).spec.postcondition.heap.pointer_declarations.size() == 0;
+        auto const &pre = std::move(goal).spec.precondition;
+        auto const &precondition_empty = pre.heap.pointer_declarations.size() == 0 && pre.heap.predicate_calls.size() == 0 && pre.heap.array_declarations.size() == 0 && pre.proposition.size() == 0;
+        auto const &post = std::move(goal).spec.postcondition;
+        auto const &postcondition_empty = post.heap.pointer_declarations.size() == 0 && post.heap.predicate_calls.size() == 0 && post.heap.array_declarations.size() == 0 && post.proposition.size() == 0;
         if (precondition_empty && postcondition_empty)
         {
             std::cout << "Using EMP" << std::endl;
