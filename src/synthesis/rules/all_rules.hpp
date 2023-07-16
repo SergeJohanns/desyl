cd7fd22b757fae2bea404a56b28b72b8f20ca14e
Serge Johanns <serge.johanns@gmail.com>
2023-07-16 20:34:12 +0200

    Add REFLEXIVE rule

--- src/synthesis/rules/all_rules.hpp
+++ src/synthesis/rules/all_rules.hpp
@@ -10,17 +10,19 @@
 #include <pure_unify.hpp>
 #include <subst_right.hpp>
 #include <pure_rewrites/pure_frame.hpp>
+#include <pure_rewrites/reflexivity.hpp>
 #include <memory>
 
 namespace desyl
 {
-    constexpr size_t RULES = 9;
+    constexpr size_t RULES = 10;
 
     // Pointers because the subclasses take up different amounts of memory
     const std::unique_ptr<Rule> all_rules[RULES] = {
         std::make_unique<EmpRule>(EmpRule()),
         std::make_unique<FrameRule>(FrameRule()),
         std::make_unique<PureFrameRule>(PureFrameRule()),
+        std::make_unique<ReflexiveRule>(ReflexiveRule()),
         std::make_unique<HeapUnifyRule>(HeapUnifyRule()),
         std::make_unique<PickRule>(PickRule()),
         std::make_unique<PureUnifyRule>(PureUnifyRule()),
---
+++ src/synthesis/rules/pure_rewrites/reflexivity.cpp
@@ -0,0 +1,41 @@
+#include <reflexivity.hpp>
+
+#include <generic_continuations.hpp>
+#include <iostream>
+
+namespace desyl
+{
+    std::vector<Derivation> ReflexiveRule::apply(Goal const &goal) const
+    {
+        std::vector<Derivation> derivations;
+        auto new_goal(goal); // Can't be copied in the switch statement because of cross-initialization.
+        for (size_t i = 0; i < goal.spec.postcondition.proposition.size(); ++i)
+        {
+            if (!std::holds_alternative<BinaryOperatorCall>(goal.spec.postcondition.proposition[i]))
+            {
+                continue;
+            }
+            auto const &call = std::get<BinaryOperatorCall>(goal.spec.postcondition.proposition[i]);
+            if (*call.lhs != *call.rhs)
+            {
+                continue;
+            }
+            switch (call.type)
+            {
+            case BinaryOperator::Eq:
+            case BinaryOperator::Leq:
+            case BinaryOperator::Geq:
+                std::cout << "Using REFLEXIVE" << std::endl;
+                new_goal.spec.postcondition.proposition.erase(new_goal.spec.postcondition.proposition.begin() + i);
+                derivations.push_back(Derivation{
+                    .goals = {new_goal},
+                    .continuation = std::make_unique<IdentityContinuation>(),
+                });
+                return derivations; // Changing the size of the postcondition vector invalidates the iterators.
+            default:
+                break;
+            }
+        }
+        return derivations;
+    }
+}
