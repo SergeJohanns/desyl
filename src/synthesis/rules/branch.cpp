9af1e072f7a7bfc617749ccde2192a0c2741a830
Serge Johanns <serge.johanns@gmail.com>
2023-07-17 01:42:43 +0200

    Remove superfluous logging

--- src/synthesis/rules/branch.cpp
+++ src/synthesis/rules/branch.cpp
@@ -36,24 +36,8 @@ namespace desyl
             std::cout << "Using BRANCH" << std::endl;
             Goal ifgoal(goal);
             ifgoal.spec.precondition.proposition.push_back(clause);
-            for (auto &postcondition : ifgoal.spec.precondition.proposition)
-            {
-                std::cout << "IF: " << stringify_expression(postcondition) << std::endl;
-            }
-            for (auto &postcondition : ifgoal.spec.postcondition.proposition)
-            {
-                std::cout << "IF POST: " << stringify_expression(postcondition) << std::endl;
-            }
             Goal elsegoal(goal);
             elsegoal.spec.precondition.proposition.push_back(negated_clause);
-            for (auto &postcondition : elsegoal.spec.precondition.proposition)
-            {
-                std::cout << "ELSE: " << stringify_expression(postcondition) << std::endl;
-            }
-            for (auto &postcondition : elsegoal.spec.postcondition.proposition)
-            {
-                std::cout << "ELSE POST: " << stringify_expression(postcondition) << std::endl;
-            }
             std::cout << (clause == negated_clause) << std::endl;
             derivations.push_back(Derivation{
                 .goals = {ifgoal, elsegoal},
