2a7aa70f275f17265e6a9e8893d79ecdba290480
Serge Johanns <serge.johanns@gmail.com>
2023-07-16 18:16:40 +0200

    Refactor proposition check

--- src/synthesis/rules/emp.cpp
+++ src/synthesis/rules/emp.cpp
@@ -8,10 +8,11 @@ namespace desyl
     std::vector<Derivation> EmpRule::apply(Goal const &goal) const
     {
         auto const &pre = std::move(goal).spec.precondition;
-        auto const &precondition_empty = pre.heap.pointer_declarations.size() == 0 && pre.heap.predicate_calls.size() == 0 && pre.heap.array_declarations.size() == 0 && pre.proposition.size() == 0;
+        auto const &precondition_empty = pre.heap.pointer_declarations.size() == 0 && pre.heap.predicate_calls.size() == 0 && pre.heap.array_declarations.size() == 0;
         auto const &post = std::move(goal).spec.postcondition;
-        auto const &postcondition_empty = post.heap.pointer_declarations.size() == 0 && post.heap.predicate_calls.size() == 0 && post.heap.array_declarations.size() == 0 && post.proposition.size() == 0;
-        if (precondition_empty && postcondition_empty)
+        auto const &postcondition_empty = post.heap.pointer_declarations.size() == 0 && post.heap.predicate_calls.size() == 0 && post.heap.array_declarations.size() == 0;
+        auto const &proposition_tautology = post.proposition.size() == 0;
+        if (precondition_empty && postcondition_empty && proposition_tautology)
         {
             std::cout << "Using EMP" << std::endl;
             auto deriv = Derivation{
