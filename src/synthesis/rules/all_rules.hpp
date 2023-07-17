7b52d39ae9204d174018d252d1df79c19a6c0b4e
Serge Johanns <serge.johanns@gmail.com>
2023-07-17 04:26:04 +0200

    Comment rule ordering

--- src/synthesis/rules/all_rules.hpp
+++ src/synthesis/rules/all_rules.hpp
@@ -5,11 +5,11 @@
 #include <heap_rewrites/frame.hpp>
 #include <statements/write.hpp>
 #include <statements/read.hpp>
+#include <statements/branch.hpp>
 #include <substitution/heap_unify.hpp>
 #include <substitution/pick.hpp>
 #include <substitution/pure_unify.hpp>
 #include <substitution/subst_right.hpp>
-#include <statements/branch.hpp>
 #include <failures/post_invalid.hpp>
 #include <failures/post_inconsistent.hpp>
 #include <pure_rewrites/pure_frame.hpp>
@@ -25,17 +25,22 @@ namespace desyl
 
     // Pointers because the subclasses take up different amounts of memory
     const std::unique_ptr<Rule> all_rules[RULES] = {
+        // (Early) termination rules
         std::make_unique<PostInconsistentRule>(PostInconsistentRule()),
         std::make_unique<EmpRule>(EmpRule()),
+        // Strictly simplifying lossless rules
         std::make_unique<FrameRule>(FrameRule()),
         std::make_unique<PureFrameRule>(PureFrameRule()),
         std::make_unique<ReflexiveRule>(ReflexiveRule()),
+        // Substitution rules
         std::make_unique<HeapUnifyRule>(HeapUnifyRule()),
         std::make_unique<PickRule>(PickRule()),
         std::make_unique<PureUnifyRule>(PureUnifyRule()),
         std::make_unique<SubstRightRule>(SubstRightRule()),
-        std::make_unique<ExpandImpliedRule>(ExpandImpliedRule()),
-        std::make_unique<PostInvalidRule>(PostInvalidRule(learned_clauses)),
+        // Expensive but nongenerating rules
+        std::make_unique<ExpandImpliedRule>(ExpandImpliedRule()),            // Can add massive amount of pure clauses
+        std::make_unique<PostInvalidRule>(PostInvalidRule(learned_clauses)), // Not expensive to run but adds branching targets
+        // Code generation rules (at the end to avoid generating code that is not used)
         std::make_unique<WriteRule>(WriteRule()),
         std::make_unique<BranchRule>(BranchRule(learned_clauses)),
         std::make_unique<ReadRule>(ReadRule()),
