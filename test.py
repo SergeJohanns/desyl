335a1bdcb378bff15a66b225097403a1d3b4a9ea
Serge Johanns <serge.johanns@gmail.com>
2024-10-03 05:29:50 +0200

    Update test script

--- test.py
+++ test.py
@@ -5,13 +5,16 @@ from alive_progress import alive_bar
 EXAMPLES_DIR = 'examples'
 SOLUTIONS_DIR = 'example-solutions'
 
+CHECKMARK = u'\u2714'
+CROSSMARK = u'\u2718'
+
 solutions = os.listdir(SOLUTIONS_DIR)
 solutions.sort()
 with alive_bar(len(solutions), enrich_print=False) as bar:
     for file in solutions:
         bar.text(f'Running {file}')
         result = subprocess.run(
-            ['./build/apps/desyl', f'{EXAMPLES_DIR}/{file}'],
+            ['./build/apps/desyl', '--file', f'{EXAMPLES_DIR}/{file}'],
             capture_output=True
         ).stdout.decode('utf-8')
 
@@ -19,9 +22,9 @@ with alive_bar(len(solutions), enrich_print=False) as bar:
             correct = f.read()
 
         if result == correct:
-            print(u'\u2714', file, 'passed')
+            print(CHECKMARK, file, 'passed')
         else:
-            print(u'\u2718', file, 'failed')
+            print(CROSSMARK, file, 'failed')
             print(f'Expected:\n{correct}')
             print(f'Got:\n{result}')
 
