6fb2883ad0256ed18a64a67cf1e5478f76720fec
Serge Johanns <serge.johanns@gmail.com>
2024-10-06 23:43:39 +0200

    Make guided search more user friendly

--- src/synthesis/proof_searchers/guided.cpp
+++ src/synthesis/proof_searchers/guided.cpp
@@ -4,67 +4,127 @@
 
 namespace desyl
 {
+    ProofTreeNode *get_rule_node(ProofTreeNode &current)
+    {
+        while (true)
+        {
+            std::string chosen_rule;
+            std::cout << "Goal: " << stringify_function_spec(current.goal.value().spec) << std::endl;
+            std::cout << "Choose a rule: ";
+            std::cin >> chosen_rule;
+            if (chosen_rule == "Back")
+            {
+                return nullptr;
+            }
+            for (auto &child : current.children)
+            {
+                if (child->rule->name() == chosen_rule)
+                {
+                    return child;
+                }
+            }
+            std::cout << "Rule not found" << std::endl;
+        }
+    }
+
+    ProofTreeNode *get_subderivation_node(ProofTreeNode &current)
+    {
+        while (true)
+        {
+            size_t chosen_subderivation;
+            if (current.goal.has_value())
+            {
+                std::cout << "Goal: " << stringify_function_spec(current.goal.value().spec) << std::endl;
+            }
+            int i = 1;
+            for (auto &child : current.children)
+            {
+                std::cout << "Subderivation " << i++ << ": " << stringify_function_spec(child->goal.value().spec) << std::endl;
+            }
+            std::cout << "Choose a subderivation: ";
+            std::cin >> chosen_subderivation;
+            if (chosen_subderivation == 0)
+            {
+                return nullptr;
+            }
+            if (chosen_subderivation > 0 && chosen_subderivation <= current.children.size())
+            {
+                return current.children[chosen_subderivation - 1];
+            }
+            std::cout << "Subderivation not found" << std::endl;
+        }
+    }
+
     void depth_first_search(ProofTreeNode const &root, ProofTreeNode &current)
     {
         if (current.children.empty())
         {
-            std::cout << "Backtracking" << std::endl;
+            std::cout << "No synthesis targets, returning" << std::endl;
             return;
         }
-        std::string chosen_rule;
-        int chosen_subderivation;
-        // Rule choice
-        if (current.children[0]->rule)
+        bool backtracked = false;
+        while (true)
         {
-            std::cout << "Goal: " << stringify_function_spec(current.goal.value().spec) << std::endl;
-            std::cout << "Choose a rule: ";
-            std::cin >> chosen_rule;
-        }
-        else if (current.is_or_node && current.children[0]->goal.has_value())
-        {
-            if (current.children.size() == 1)
+            ProofTreeNode *chosen_node = nullptr;
+            if (current.children[0]->rule)
             {
-                chosen_subderivation = 1;
+                // Choose what rule to apply
+                chosen_node = get_rule_node(current);
+                if (chosen_node == nullptr)
+                {
+                    std::cout << "Backtracking" << std::endl;
+                    return;
+                }
+            }
+            else if (current.is_or_node && current.children.size() > 1)
+            {
+                // Choose what subderivation to follow
+                chosen_node = get_subderivation_node(current);
+                if (chosen_node == nullptr)
+                {
+                    std::cout << "Backtracking" << std::endl;
+                    return;
+                }
+            }
+            else if (backtracked)
+            {
+                // If the user hasn't chosen a rule or subderivation, and we already backtracked to this node, then we need to backtrack further
+                return;
+            }
+            else if (current.is_or_node)
+            {
+                // This means we are in a rule node that only has one child (sometimes this is an AND node)
+                chosen_node = current.children[0];
+            }
+
+            if (chosen_node == nullptr)
+            {
+                for (auto &child : current.children)
+                {
+                    // Rules can create expanded children, so we need to check this here
+                    if (!child->expanded)
+                    {
+                        child->expand();
+                    }
+                    depth_first_search(root, *child);
+                }
             }
             else
             {
-                if (!current.rule)
+                // Rules can create expanded children, so we need to check this here
+                if (!chosen_node->expanded)
                 {
-                    std::cout << "Goal: " << stringify_function_spec(current.goal.value().spec) << std::endl;
+                    chosen_node->expand();
                 }
-                int i = 1;
-                for (auto &child : current.children)
-                {
-                    std::cout << "Subderivation " << i++ << ": " << stringify_function_spec(child->goal.value().spec) << std::endl;
-                }
-                std::cout << "Choose a subderivation: ";
-                std::cin >> chosen_subderivation;
+                depth_first_search(root, *chosen_node);
             }
-        }
-        int i = 1;
-        for (auto &child : current.children)
-        {
-            bool expanded = false;
-            if (root.completed || current.completed)
+            if (current.completed)
             {
                 return;
             }
-            if (!child->expanded)
-            {
-                if (child->rule->name() != chosen_rule)
-                {
-                    continue;
-                }
-                std::cout << "Using " << child->rule->name() << std::endl;
-                // Rules can create expanded children, so we need to check this here
-                child->expand();
-                expanded = true;
-            }
-            if (expanded || chosen_subderivation == 0 || i == chosen_subderivation)
-            {
-                depth_first_search(root, *child);
-            }
-            i++;
+            // Once we have descended into a child and come back, we must have backtracked, so we need to check if we need to backtrack further
+            // It's also possible that a rule was chosen that didn't create any children, but if so this is a rule decision node, so we won't backtrack anyways
+            backtracked = true;
         }
     }
 
