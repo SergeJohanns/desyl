b13ac66b2c0a9d4beb7f0a2e6cd856cafe18b474
Serge Johanns <serge.johanns@gmail.com>
2024-11-26 10:05:30 +0100

    Allow training ML on simplified trees

--- simplify_tree.py
+++ simplify_tree.py
@@ -4,23 +4,33 @@ JSON_FILE = "proof_tree.json"
 OUTPUT_FILE = "simplified_tree.json"
 PROOF_ONLY = True
 
-with open(JSON_FILE, "r") as f:
-    tree = json.load(f)
 
 def simplify_tree(tree):
     tree["children"] = [child for child in tree["children"] if child["children"]]
     for child in tree["children"]:
         simplify_tree(child)
 
+
 def simplify_to_proof(tree):
     for child in tree["children"]:
         simplify_to_proof(child)
-    tree["children"] = [child for child in tree["children"] if child.get("completed", False)]
+    tree["children"] = [
+        child for child in tree["children"] if child.get("completed", False)
+    ]
 
-if PROOF_ONLY:
-    simplify_to_proof(tree)
-else:
-    simplify_tree(tree)
 
-with open(OUTPUT_FILE, "w") as f:
-    json.dump(tree, f, indent=2)
+def simplify_file(tree_file, output_file, proof_only=True):
+    with open(tree_file, "r") as f:
+        tree = json.load(f)
+
+    if proof_only:
+        simplify_to_proof(tree)
+    else:
+        simplify_tree(tree)
+
+    with open(output_file, "w") as f:
+        json.dump(tree, f, indent=2)
+
+
+if __name__ == "__main__":
+    simplify_file(JSON_FILE, OUTPUT_FILE, PROOF_ONLY)
--- train_dt.py
+++ train_dt.py
@@ -6,6 +6,7 @@ import pandas as pd
 import ydf
 from alive_progress import alive_it
 from benchmark import Synthesizer, ResultType, TREEOUT
+from simplify_tree import simplify_file
 
 GOAL_REGEX = re.compile(r"{(.*);} -> {(.*);}")
 ASSERTION_REGEX = re.compile(r"(.*); (.*)")
@@ -15,6 +16,33 @@ PREDICATE_REGEX = re.compile(r"(\w+)\((.*)\)")
 
 EXAMPLES_DIR = "examples"
 EXCLUDE = ["pick.sep"]
+SIMPLIFY = False
+
+ALL_RULES = {
+    "PostInconsistent",
+    "SetSize",
+    "Emp",
+    "NullNotLVal",
+    "TrueElision",
+    "Frame",
+    "PureFrame",
+    "Reflexive",
+    "ExpandImplied",
+    "Open",
+    "Close",
+    "HeapUnify",
+    "Pick",
+    "PureUnify",
+    "SubstRight",
+    "PostInvalid",
+    "Read",
+    "Write",
+    "Free",
+    "Alloc",
+    "Call",
+    "AbduceCall",
+    "Branch",
+}
 
 
 def get_assertion_features(assertion: str) -> dict[str, int | str]:
@@ -38,35 +66,47 @@ def get_goal_features(goal: str) -> dict[str, int | str]:
     return precondition_features | postcondition_features
 
 
-def get_rule_node_data(goal: str, node: dict) -> list[dict]:
-    row = {
+def make_rule_node_data(goal: str, rule: dict, completed: bool) -> dict:
+    return {
         **get_goal_features(goal),
-        "rule": node["rule"],
+        "rule": rule,
         "score": int(
-            node.get("completed", False)
+            completed
         ),  # 0 or 1, system will infer probability of leading to completion
     }
+
+
+def get_rule_node_data(goal: str, node: dict, add_missing_rules: bool) -> list[dict]:
+    row = make_rule_node_data(goal, node["rule"], node.get("completed", False))
     child_rows = list(
         itertools.chain.from_iterable(
-            get_goal_node_data(child) for child in node["children"]
+            get_goal_node_data(child, add_missing_rules) for child in node["children"]
         )
     )
     return [row] + child_rows
 
 
-def get_goal_node_data(node: dict) -> list[dict]:
+def get_goal_node_data(node: dict, add_missing_rules: bool) -> list[dict]:
     if "goal" not in node:
         return list(
             itertools.chain.from_iterable(
-                get_goal_node_data(child) for child in node["children"]
+                get_goal_node_data(child, add_missing_rules)
+                for child in node["children"]
             )
         )
     else:
-        return list(
+        child_rows = list(
             itertools.chain.from_iterable(
-                get_rule_node_data(node["goal"], child) for child in node["children"]
+                get_rule_node_data(node["goal"], child, add_missing_rules)
+                for child in node["children"]
             )
         )
+        if add_missing_rules:
+            child_rows += [
+                make_rule_node_data(node["goal"], rule, False)
+                for rule in ALL_RULES - {child["rule"] for child in node["children"]}
+            ]
+        return child_rows
 
 
 def precision(y_true, y_pred):
@@ -97,10 +137,14 @@ for file in bar:
         print(f"Failed to synthesize {file} due to {result_type.value}")
         continue
 
-    with open(TREEOUT, "r") as f:
+    simplified_file = f"simplified_{TREEOUT}"
+    if SIMPLIFY:
+        simplify_file(TREEOUT, simplified_file, proof_only=True)
+
+    with open(simplified_file if SIMPLIFY else TREEOUT, "r") as f:
         tree = json.load(f)
 
-    row_list.extend(get_goal_node_data(tree))
+    row_list.extend(get_goal_node_data(tree, add_missing_rules=SIMPLIFY))
 
 
 if __name__ == "__main__":
